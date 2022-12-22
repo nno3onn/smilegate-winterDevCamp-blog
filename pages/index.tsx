32b9c9512b5523fac7cbaec62edbc6e8bf4a68d6
nno3onn <nno3onn@naver.com>
2022-12-22 20:31:10 +0900

    feat, give type to Home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
+import type { NextPage } from "next";
 import styled from "styled-components";
 import Nav from "../components/nav/Nav";
 import MainPage from "../containers/MainPage";
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <Container>
       <Nav />
       <MainPage />
     </Container>
   );
-}
+};
 const Container = styled.div`
   background-color: #f8f9fa;
 `;
+
+export default Home;
