b281b029fdc761362e98fed6672b7e93d08c8ba3
nno3onn <nno3onn@naver.com>
2022-12-23 10:30:57 +0900

    refac, remove return in updateComment

--- util/api/updateComment.ts
+++ util/api/updateComment.ts
@@ -2,11 +2,10 @@ const axios = require("axios");
 
 const updateComment = async ({ content, comment_id }: { content: string; comment_id: number }) => {
   try {
-    const res = await axios.patch(`http://localhost:3000/api/comment`, {
+    await axios.patch(`http://localhost:3000/api/comment`, {
       content,
       comment_id,
     });
-    return res.data.data;
   } catch (err) {
     console.log(err);
     alert(err.response?.data.message);
