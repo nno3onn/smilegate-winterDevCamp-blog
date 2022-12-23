79e768e33d95ef5ad2512d868961523cdbe3aacc
nno3onn <nno3onn@naver.com>
2022-12-24 02:39:38 +0900

    feat, add props type

--- util/api/createPost.ts
+++ util/api/createPost.ts
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const createPost = async ({ title, content, thumbnail = null }) => {
+const createPost = async ({ title, content, thumbnail = null }: { title: string; content: string; thumbnail: string }) => {
   try {
     const res = await axios.post(`http://localhost:3000/api/post`, {
       title,
