1851b36c444da6c755668424117690b8ce96c63e
nno3onn <nno3onn@naver.com>
2022-12-24 13:55:13 +0900

    feat, implement favicon

--- components/Helmet.tsx
+++ components/Helmet.tsx
@@ -5,7 +5,7 @@ const Helmet = ({ title }: { title: string }) => (
     <title>{title}</title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1, user-scalable=no" />
-    <link rel="shortcut icon" href="/apple.png" />
+    <link rel="shortcut icon" href="/favicon.png" />
   </Head>
 );
 
