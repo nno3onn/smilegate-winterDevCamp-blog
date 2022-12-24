49040d32634a0333c9d5f5928d265a86965c9b02
nno3onn <nno3onn@naver.com>
2022-12-24 21:34:11 +0900

    refac, remove useless file

--- util/getIsAdminByUserId.ts
+++
@@ -1,8 +0,0 @@
-// import isAdmin from "./api/isAdmin";
-
-// const getIsAdminByUserId = async (user_id: number) => {
-//   const res = await isAdmin(user_id);
-//   return res;
-// };
-
-// export default getIsAdminByUserId;
