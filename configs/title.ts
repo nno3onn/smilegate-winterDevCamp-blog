a193af31702370172a65ecf637f432b0c458c5a4
nno3onn <nno3onn@naver.com>
2022-12-24 15:22:06 +0900

    etc, change title

--- configs/title.ts
+++ configs/title.ts
@@ -1,7 +1,7 @@
 const mainTitle = "Applog | 메인 페이지";
-const writeTitle = "Applog | 새 글 작성";
-const postTitle = "Applog | 글 보기";
-const editTitle = "Applog | 작성 중";
+const writeTitle = "Applog | 새 포스트 작성";
+const postTitle = "Applog | 포스트 보기";
+const editTitle = "Applog | 포스트 작성 중";
 
 const titleConfigs = {
   mainTitle,
