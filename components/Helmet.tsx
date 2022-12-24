1bdd7f8d415222e2251f5ccf1a5edeaf41fe3b5e
nno3onn <nno3onn@naver.com>
2022-12-24 13:55:00 +0900

    feat, implement favicon

--- components/Helmet.tsx
+++ components/Helmet.tsx
@@ -1,10 +1,12 @@
 import Head from "next/head";
 
-const Helmet = ({ title }) => {
+const Helmet = ({ title }: { title: string }) => (
   <Head>
     <title>{title}</title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1, user-scalable=no" />
-    <link rel="shortcut icon" href="/favicon.ico" />
-  </Head>;
-};
+    <link rel="shortcut icon" href="/apple.png" />
+  </Head>
+);
+
+export default Helmet;
