c2802669dd651d5960ba65c96a9d3ed298ff95ad
nno3onn <nno3onn@naver.com>
2022-12-26 12:34:11 +0900

    fix, remove crossOrigin

--- components/Helmet.tsx
+++ components/Helmet.tsx
@@ -7,7 +7,7 @@ const Helmet = ({ title }: { title: string }) => (
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1, user-scalable=no" />
     <link rel="shortcut icon" href="/favicon.png" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Martian+Mono&display=swap" rel="stylesheet" />
   </Head>
 );
